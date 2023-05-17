787e459e7b6e1fb0eff7584ae011dda88e1499a3
Administrator <1850195602@qq.com>
2023-05-17 21:09:39 +0800

    拦截器拦截路径

--- dcloud-common/src/main/java/net/xdclass/interceptor/LoginInterceptor.java
+++ dcloud-common/src/main/java/net/xdclass/interceptor/LoginInterceptor.java
@@ -14,7 +14,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.net.http.HttpRequest;
 
 public class LoginInterceptor implements HandlerInterceptor {
     private static ThreadLocal<LoginUser> threadLocal= new ThreadLocal<>();
