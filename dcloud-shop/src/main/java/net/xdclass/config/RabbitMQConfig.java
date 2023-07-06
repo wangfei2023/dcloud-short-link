4155db108511084c0c58dc2b69e882d39f3df4d7
Administrator <1850195602@qq.com>
2023-07-06 22:18:03 +0800

    实现延迟消息配置

---
+++ dcloud-shop/src/main/java/net/xdclass/config/RabbitMQConfig.java
@@ -0,0 +1,128 @@
+/**
+ * @project dcloud-short-link
+ * @description 自定义消息队列配置
+ *  发送 关单消息-》延迟exchange-》order.close.delay.queue-》死信exchange-》
+ * @author Administrator
+ * @date 2023/7/6 0006 21:17:00
+ * @version 1.0
+ */
+
+package net.xdclass.config;
+
+import lombok.Data;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@Data
+public class RabbitMQConfig {
+    /**
+     * 交换机
+     */
+    private String orderEventExchange="order.event.exchange";
+
+
+    /**
+     * 延迟队列, 不能被监听消费
+     */
+    private String orderCloseDelayQueue="order.close.delay.queue";
+
+    /**
+     * 关单队列, 延迟队列的消息过期后转发的队列,被消费者监听
+     */
+    private String orderCloseQueue="order.close.queue";
+
+
+    /**
+     * 进入延迟队列的路由key
+     */
+    private String orderCloseDelayRoutingKey="order.close.delay.routing.key";
+
+
+    /**
+     * 进入死信队列的路由key，消息过期进入死信队列的key
+     */
+    private String orderCloseRoutingKey="order.close.routing.key";
+
+    /**
+     * 过期时间 毫秒，临时改为1分钟定时关单
+     */
+    private Integer ttl=1000*60;
+
+    /**
+     * 消息转换器
+     * @return
+     */
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+
+    /**
+     * 创建交换机 Topic类型，也可以用dirct路由
+     * 一般一个微服务一个交换机
+     * @return
+     */
+    @Bean
+    public Exchange orderEventExchange(){
+        return new TopicExchange(orderEventExchange,true,false);
+    }
+
+
+    /**
+     * 延迟队列
+     */
+    @Bean
+    public Queue orderCloseDelayQueue(){
+
+        Map<String,Object> args = new HashMap<>(3);
+        args.put("x-dead-letter-exchange",orderEventExchange);
+        args.put("x-dead-letter-routing-key",orderCloseRoutingKey);
+        args.put("x-message-ttl",ttl);
+
+        return new Queue(orderCloseDelayQueue,true,false,false,args);
+    }
+
+
+    /**
+     * 死信队列，普通队列，用于被监听
+     */
+    @Bean
+    public Queue orderCloseQueue(){
+
+        return new Queue(orderCloseQueue,true,false,false);
+
+    }
+
+    /**
+     * 第一个队列，即延迟队列的绑定关系建立
+     * @return
+     */
+    @Bean
+    public Binding orderCloseDelayBinding(){
+
+        return new Binding(orderCloseDelayQueue,Binding.DestinationType.QUEUE,orderEventExchange,orderCloseDelayRoutingKey,null);
+    }
+
+    /**
+     * 死信队列绑定关系建立
+     * @return
+     */
+    @Bean
+    public Binding orderCloseBinding(){
+
+        return new Binding(orderCloseQueue,Binding.DestinationType.QUEUE,orderEventExchange,orderCloseRoutingKey,null);
+    }
+}
+
+
